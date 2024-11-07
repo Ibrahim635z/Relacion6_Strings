2dc73faa72cc2fc6230b071fece7cf1567581f3b
Ibrahim635z <imohahm635z@g.educaand.es>
2024-11-07 13:11:07 +0100

    final

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -6,13 +6,14 @@ public class Ejercicio6 {
     public static void main(String[] args) {
 
         String cadena="", cadena_invertida="";
+        
         Scanner entrada=new Scanner(System.in);
         
         System.out.println("Introduzca la frase que desea invertir");
         cadena=entrada.nextLine();
         
         for (int i = cadena.length()-1; i>=0; i--) {
-            cadena_invertida+=cadena.charAt(i);
+           cadena_invertida+=cadena.charAt(i);
           
         }
 
